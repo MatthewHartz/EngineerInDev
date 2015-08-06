6b6a3aa8ac1c6798abb558090ef39fe75c2eb162
rvbmrdonut <hartzm@uci.edu>
2015-08-05 20:19:52 -0700

    Save point
    Changed about me to not use a stored blog instead just rely on a view.
    
    Changed color design of site
    
    Save point before a ton of css changes.

--- EngineerInDev/Content/Scripts/Controllers/AboutCtrl.js
+++ EngineerInDev/Content/Scripts/Controllers/AboutCtrl.js
@@ -1,23 +1,5 @@
 ﻿app.controller('AboutCtrl', ['$scope', '$http', '$sce', function ($scope, $http, $sce) {
-    $scope.blog = {};
-
-    $scope.init = function () {
-        $http({
-            url: '/api/blogs/',
-            method: "GET",
-            params: { name : 'About Me'}
-            })
-            .success(function (blog) {
-                $scope.blog = blog;
-                $scope.blog['htmlBody'] = $sce.trustAsHtml(blog.content);
-
-                $scope.identifier = blog.title;
-                $scope.url = 'http://localhost:60664/#!/Blogs/' + blog.title;
-                $scope.contentLoaded = true;
-            }).error(function () {
-                var test = '';
-            });
-    };
-
-    $scope.init();
+    $scope.identifier = "About Matthew Hartz";
+    $scope.url = 'http://localhost:60664/About';
+    $scope.contentLoaded = true;
 }]);
